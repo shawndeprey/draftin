e7133f4c921c9aaf0ec2ae213940ac90e2c96a88
Shawn Deprey <shawndeprey@gmail.com>
2014-02-28 15:42:01 -0800

    Added database models.

---
+++ db/migrate/20140228220537_create_base_models.rb
@@ -0,0 +1,97 @@
+class CreateBaseModels < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.integer :position
+      # has_many_drafts
+      # has_many_cards
+      # has_many_packs
+      t.timestamps
+    end
+
+    create_table :cards do |t|
+      t.string :layout # "Normal"
+      t.string :name # "Research // Development"
+      t.string :mana_cost # "{G}{U}"
+      t.integer :cmc # 2 Converted Mana Cost
+      t.string :colors # "Blue, Red"
+      t.string :type # "Legendary Creature - Angel"
+      t.string :supertypes # "Legendary"
+      t.string :types # "Creature"
+      t.string :subtypes # "Angel"
+      t.string :rarity # "Rare"
+      t.text :text # "{T}: You gain 1 life."
+      t.text :flavor # "I'd like to buy a bowel."
+      t.string :artist # "Mark Poole"
+      t.string :number # "148a"
+      t.integer :power # 4
+      t.integer :toughness # 5
+      t.integer :loyalty # 4
+      t.integer :multiverseid # 2479
+      t.string :variations # "78968, 85106"
+      t.string :image_name # "ajani goldmane"
+      t.string :watermark # "Selesnya"
+      t.string :border # "black"
+      t.integer :hand # -3 Maximum hand size modifier. Only exists for Vanguard cards.
+      t.integer :life # -10 Starting life total modifier. Only exists for Vanguard cards.
+      t.timestamps
+    end
+
+    create_table :drafts do |t|
+      t.string :name
+      t.integer :user_count
+      # has_many_users
+      # has_many_sets
+      t.timestamps
+    end
+
+    create_table :packs do |t|
+      t.integer :set_id
+      t.integer :order_received # Order user received the pack in
+      # has_one_set
+      # has_and_belongs_to_many_cards
+      t.timestamps
+    end
+
+    create_table :sets do |t|
+      t.string :short_name # "ARB"
+      t.string :name # "Alara Reborn"
+      t.string :border # "black"
+      t.string :type # "expansion"
+      t.string :url # "https://api.deckbrew.com/mtg/sets/ARB"
+      t.string :cards_url # "https://api.deckbrew.com/mtg/cards?set=ARB"
+      t.timestamps
+    end
+
+    create_table :draft_sets do |t|
+      t.integer :draft_id
+      t.integer :set_id
+      t.timestamps
+    end
+
+    create_table :draft_users do |t|
+      t.integer :draft_id
+      t.integer :user_id
+      t.timestamps
+    end
+
+    create_table :user_cards do |t|
+      t.integer :user_id
+      t.integer :card_id
+      t.timestamps
+    end
+
+    create_table :user_packs do |t|
+      t.integer :user_id
+      t.integer :pack_id
+      t.timestamps
+    end
+
+    create_table :pack_cards do |t|
+      t.integer :pack_id
+      t.integer :card_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,106 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140228220537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: true do |t|
+    t.string   "layout"
+    t.string   "name"
+    t.string   "mana_cost"
+    t.integer  "cmc"
+    t.string   "colors"
+    t.string   "type"
+    t.string   "supertypes"
+    t.string   "types"
+    t.string   "subtypes"
+    t.string   "rarity"
+    t.text     "text"
+    t.text     "flavor"
+    t.string   "artist"
+    t.string   "number"
+    t.integer  "power"
+    t.integer  "toughness"
+    t.integer  "loyalty"
+    t.integer  "multiverseid"
+    t.string   "variations"
+    t.string   "image_name"
+    t.string   "watermark"
+    t.string   "border"
+    t.integer  "hand"
+    t.integer  "life"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "draft_sets", force: true do |t|
+    t.integer  "draft_id"
+    t.integer  "set_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "draft_users", force: true do |t|
+    t.integer  "draft_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "drafts", force: true do |t|
+    t.string   "name"
+    t.integer  "user_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pack_cards", force: true do |t|
+    t.integer  "pack_id"
+    t.integer  "card_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "packs", force: true do |t|
+    t.integer  "set_id"
+    t.integer  "order_received"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sets", force: true do |t|
+    t.string   "short_name"
+    t.string   "name"
+    t.string   "border"
+    t.string   "type"
+    t.string   "url"
+    t.string   "cards_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_cards", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "card_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_packs", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "pack_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
