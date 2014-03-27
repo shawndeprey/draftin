e9b8b8925101d39d70cbb0bf3f756ca728dac5c1
Shawn Deprey <shawndeprey@gmail.com>
2014-03-26 20:19:28 -0700

    Changed default to 8 for max users

--- db/migrate/20140327003138_add_max_users_to_drafts.rb
+++ db/migrate/20140327003138_add_max_users_to_drafts.rb
@@ -1,5 +1,5 @@
 class AddMaxUsersToDrafts < ActiveRecord::Migration
   def change
-    add_column :drafts, :max_users, :integer, default: 1
+    add_column :drafts, :max_users, :integer, default: 8
   end
 end
